a940cb678b3e6ec475e94d35201e0b2951d30071
gjianbo <gjianbo@qq.com>
2021-06-18 10:11:56 +0800

    加入对float 类型的转换

--- validators.go
+++ validators.go
@@ -1117,6 +1117,8 @@ func convert(val interface{}) (value interface{}, err error) {
 		value = v.Int()
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		value = v.Uint()
+	case reflect.Float64, reflect.Float32:
+		value = int64(val.(float64))
 	default:
 		err = errConvertFail
 	}
