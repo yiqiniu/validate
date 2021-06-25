f0f11f3b99cb79f6d57515ac62fe784b33217cd8
gjianbo <gjianbo@qq.com>
2021-06-25 11:29:50 +0800

    加入required  对int 或float 转换为字符串进行比较结果

--- util.go
+++ util.go
@@ -87,11 +87,11 @@ func ValueIsEmpty(v reflect.Value) bool {
 	case reflect.Bool:
 		return !v.Bool()
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
+		return strconv.FormatInt(v.Int(), 10) == ""
 	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
+		return strconv.FormatUint(v.Uint(), 10) == ""
 	case reflect.Float32, reflect.Float64:
-		return v.Float() == 0
+		return strconv.FormatFloat(v.Float(), 'f', 2, 32) == ""
 	case reflect.Interface, reflect.Ptr:
 		return v.IsNil()
 	}
